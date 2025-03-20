ad73d3b612845fd8bdacebbaa793652516da5f33
rabgc <tectorab@gmail.com>
2025-03-20 18:12:57 -0400

    Added assertions to check matrix dimensions and sizes upon instantiation in Kalman.cpp and changed symbols used in UnitTest.cpp.

--- Kalman.cpp
+++ Kalman.cpp
@@ -9,7 +9,20 @@
 KalmanFilter::KalmanFilter(const Eigen::MatrixXd& S, const Eigen::MatrixXd& F,
                            const Eigen::MatrixXd& Q, const Eigen::MatrixXd& R,
                            const Eigen::MatrixXd& P, const Eigen::VectorXd& x)
-    : S_(S), F_(F), Q_(Q), R_(R), P_(P), x_(x) {}
+    : S_(S), F_(F), Q_(Q), R_(R), P_(P), x_(x) {
+
+    // Ensure matrix dimensions are consistent
+    assert(S_.rows() == S_.cols() && "State trans S must be square.");
+    assert(S_.rows() == x_.size() && "State trans S and state x need same dim");
+    assert(P_.rows() == P_.cols() && "Cov P must be square");
+    assert(P_.rows() == x_.size() && "Cov P and state x need same dim");
+    assert(Q_.rows() == Q_.cols() && "Noise cov Q must be square");
+    assert(Q_.rows() == S_.rows() && "Noise cov Q and State trans S need same dim");
+    assert(F_.cols() == x_.size() && "Features F need same no. of cols as state x");
+    assert(R_.rows() == R_.cols() && "Obs err cov R must be square.");
+    assert(F_.rows() == R_.rows() && "Feature F and obs err cov R need same dim");
+
+    }
 
 void KalmanFilter::predict() {
         x_ = S_ * x_;                        // propagate the state
@@ -18,16 +31,21 @@ void KalmanFilter::predict() {
 
 void KalmanFilter::update(const Eigen::VectorXd& z) {
 
-        Eigen::VectorXd y = z - F_ * x_;   // innovations 
-        Eigen::MatrixXd C = F_ * P_ * F_.transpose() + R_; // innovations cov
-        Eigen::MatrixXd K = P_ * F_.transpose() * \
-        C.ldlt().solve(Eigen::MatrixXd::Identity(C.rows(), C.cols())); // Kalman gain
+        // Innovations 
+        Eigen::VectorXd y = z - F_ * x_;   
 
-        x_ = x_ + K * y;  // state update 
+        // Innovations covariance
+        Eigen::MatrixXd C = F_ * P_ * F_.transpose() + R_; 
+
+        // Kalman gain
+        Eigen::MatrixXd K = P_ * F_.transpose() * \
+        C.ldlt().solve(Eigen::MatrixXd::Identity(C.rows(), C.cols())); 
+
+        // Update the state estimate
+        x_ = x_ + K * y;  
         
         // The traditional covariance update can be unstable  
         // P_ = (Eigen::MatrixXd::Identity(P_.rows(), P_.cols()) - K * F_) * P_;  
-        
         // So we use Joseph's stabilized update (cf. Bierman, 1977)
         Eigen::MatrixXd I = Eigen::MatrixXd::Identity(P_.rows(), P_.cols());
         P_ = (I - K * F_) * P_ * (I - K * F_).transpose() + K * R_ * K.transpose();
--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -8,63 +8,90 @@
 // Multipe stacked n-dimensional state vectors 
 int main() {
     
-    // dynamics 
-    int physical_dimensions = 3; // e.g., x, y, z
-    int system_order = 2; //  1 or 2 for first order or second order
+    /**  
+     * System dynamics  
+     */ 
 
-    // system state 
-    int state_dimensions = physical_dimensions * system_order; // e.g., 6 for 3D state 
-    int num_targets = 2; // e.g., GRPs or SV CMs 
+    // E.g., x, y, z
+    int physical_dimensions = 3; 
+
+    // 1 or 2 for first or second order ODE
+    int system_order = 2; 
+
+    /**
+     * System state
+     */
+
+    // e.g., 6 for 3D state and 2nd order dynamics 
+    int state_dimensions = physical_dimensions * system_order; 
+
+    // E.g., GRPs or SV CMs 
+    int num_targets = 2; 
+
+    // Total length of the data vector at a single nepoch
     int stacked_state_size = state_dimensions * num_targets;
 
-    // tracking data
-    int num_epochs = 10; 
-    int num_obs_dimensions = 3; // e.g., x, y, z
-    int num_obs_per_epoch = num_obs_dimensions * num_targets; // e.g., 6 for 3 obs dimensions and 2 targets
-    int num_stacked_obs = num_epochs * num_obs_per_epoch; // e.g., 18 for 3 epochs and 6 obs per epoch 
+    /** 
+     * Tracking data
+     * */ 
 
-    // constant unit sampling
+    // Experiment with how few epochs required to track targets  
+    int num_epochs = 10; 
+
+    // assume obs dimensions are the same as physical dimensions
+    int num_obs_dimensions = physical_dimensions; 
+
+    // E.g., 6 for 3 obs dimensions and 2 targets
+    int num_obs_per_epoch = num_obs_dimensions * num_targets; 
+
+    // E.g., 18 for 3 epochs and 6 obs per epoch 
+    int num_stacked_obs = num_epochs * num_obs_per_epoch; 
+
+    // Defines the time units 
     double dt = 2.0; 
 
     // State transition matrix
-    Eigen::MatrixXd Feye = Eigen::MatrixXd::Identity(physical_dimensions, physical_dimensions); 
-    Eigen::MatrixXd F(stacked_state_size, stacked_state_size);
+    Eigen::MatrixXd Seye = Eigen::MatrixXd::Identity(physical_dimensions, physical_dimensions); 
+    Eigen::MatrixXd S(stacked_state_size, stacked_state_size);
     for (int i =0; i < num_targets; i++) {
 
         // the first row of each target 
         int idx = i * state_dimensions; // 0, 6, 12 
 
         // positions 
-        F.block(idx, idx, physical_dimensions, physical_dimensions) = Feye;
+        S.block(idx, idx, physical_dimensions, physical_dimensions) = Seye;
         
-        // apply rate terms 
+        // Apply rate terms 
         for (int j = 1; j < system_order; j++) {
 
             int jdx = idx + j * physical_dimensions; // 3, 6, 9
-            F.block(idx, jdx, physical_dimensions, physical_dimensions) = std::pow(dt, j) / std::tgamma(j+1) * Feye;
+            S.block(idx, jdx, physical_dimensions, physical_dimensions) = \
+                std::pow(dt, j) / std::tgamma(j+1) * Seye;
 
-            // the identity part 
-            F.block(idx + j * physical_dimensions, idx + j * physical_dimensions, physical_dimensions, physical_dimensions) = Feye;
+            // The identity part 
+            S.block(idx + j * physical_dimensions, idx + \
+                j * physical_dimensions, physical_dimensions, physical_dimensions) \
+                = Seye;
         }
     }
-    std::cout << "State Transition Matrix F:\n" << F << std::endl;
+    std::cout << "State Transition Matrix S:\n" << S << std::endl;
 
-    // Design matrix
-    Eigen::MatrixXd H = Eigen::MatrixXd::Zero(num_obs_per_epoch, stacked_state_size);
+    // Features matrix
+    Eigen::MatrixXd F = Eigen::MatrixXd::Zero(num_obs_per_epoch, stacked_state_size);
     for (int i = 0; i < num_targets; i++) {
         int idx = i * state_dimensions;
         std::cout << "indices" << i * num_obs_dimensions << idx <<  ":\n";
-        H(i * num_obs_dimensions, idx) = 1; // observe x
-        H(i * num_obs_dimensions + 1, idx + 1) = 1; // observe y
-        H(i * num_obs_dimensions + 2, idx + 2) = 1; // observe z
+        F(i * num_obs_dimensions, idx) = 1;         // observe x
+        F(i * num_obs_dimensions + 1, idx + 1) = 1; // observe y
+        F(i * num_obs_dimensions + 2, idx + 2) = 1; // observe z
     }
-    std::cout << "Design matrix:\n" << H << "\n";
+    std::cout << "Design matrix:\n" << F << "\n";
 
     Eigen::MatrixXd Q = Eigen::MatrixXd::Identity(stacked_state_size, stacked_state_size) * 0.01;
     Eigen::MatrixXd R = Eigen::MatrixXd::Identity(num_obs_per_epoch, num_obs_per_epoch) * 0.1;
     Eigen::MatrixXd P = Eigen::MatrixXd::Identity(stacked_state_size, stacked_state_size);
     Eigen::VectorXd x = Eigen::VectorXd::Zero(stacked_state_size);
-    KalmanFilter kf(F, H, Q, R, P, x);
+    KalmanFilter kf(S, F, Q, R, P, x);
 
     // Run the simulation
     Eigen::VectorXd x_true = Eigen::VectorXd::Random(stacked_state_size) * 10.0;    
@@ -73,20 +100,20 @@ int main() {
     for (int iepoch = 0; iepoch < num_epochs; ++iepoch) {
 
         // propagate the true state
-        x_true = F * x_true; 
+        x_true = S * x_true; 
 
         // Simulate noisy data
         Eigen::VectorXd noise = Eigen::VectorXd::Random(num_obs_per_epoch);
         std::cout << "Noise:\n" << noise << "\n";
-        Eigen::VectorXd d = H * x_true + 0.1 * noise; 
-        std::cout << "Simulated data:\n" << d << "\n";
+        Eigen::VectorXd y = F * x_true + 0.1 * noise; 
+        std::cout << "Simulated data:\n" << y << "\n";
 
         // Kalman filter prediction and update
         kf.predict();
         std::cout << "Predicted State:\n" << kf.getState() << "\n";
         std::cout << "Predicted Covariance:\n" << kf.getCovariance() << "\n";
 
-        kf.update(d);
+        kf.update(y);
 
         // Report the results
         std::cout << "Final Updated State:\n" << kf.getState() << "\n";
